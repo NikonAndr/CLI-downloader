a7ce2646ca18d6bc7a38efd1fc3271adaf6767ce
Andrii <andrii@Andriis-MacBook-Pro-2.local>
2026-04-16 18:37:14 +0200

    HttpClient refactor, added CurlContect, header_cb

--- include/core/http/HttpClient.h
+++ include/core/http/HttpClient.h
@@ -8,5 +8,5 @@ class HttpClient
 public:
     HttpClient() = default;
     ~HttpClient() = default;
-    void download(const std::string& url, const std::function<void(const char* data, size_t size)>& callback);
+    void download(const std::string& url, const std::function<void(const char* data, size_t size)>& onData, const std::function<void(const char* data, size_t size)>& onHeader);
 };
