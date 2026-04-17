5b0836b27e4759dbe0b7eae09a42db7cf8105cfe
Andrii <andrii@Andriis-MacBook-Pro-2.local>
2026-04-17 17:41:34 +0200

    Downloader refactor: uses key-value pairs instead of substring + find method

--- src/core/download/Downloader.cpp
+++ src/core/download/Downloader.cpp
@@ -11,38 +11,50 @@ void Downloader::download(const std::string& url, const std::string& output_path
     size_t total_size = 0;
     size_t downloaded_bytes = 0;
 
-    auto data_callback = [&fw, &downloaded_bytes](const char* data, size_t size)
+    auto data_callback = [&fw, &downloaded_bytes, &total_size](const char* data, size_t size)
     {
         downloaded_bytes += size;
         fw.write(data, size);
         //Debug
-        std::cout << "Downloaded: " << downloaded_bytes << " bytes\n";
+        std::cout << "Downloaded: " << downloaded_bytes << "/" << total_size << " bytes\n";
     };
 
     auto header_callback = [&total_size](const char* buffer, size_t size)
     {
         std::string buffer_str(buffer, size);
 
-        if (buffer_str.find("Content-Length:") != std::string::npos)
-        {
-            size_t colon_pos = buffer_str.find(":");
-            if (colon_pos == std::string::npos)
+        //debug
+        std::cout << "header_cb: " << buffer_str <<" \n";
+        
+        size_t colon_pos = buffer_str.find(":");
+        if (colon_pos == std::string::npos)
                 return;
 
-            std::string content_length = buffer_str.substr(colon_pos + 1);
+        std::string key = buffer_str.substr(0, colon_pos);
+        std::string value = buffer_str.substr(colon_pos + 1);
+        
+        for (auto& c : key)
+        {
+            c = tolower(static_cast<unsigned char>(c));
+        }
 
-            size_t start = content_length.find_first_not_of(" \t\n\r");
-            size_t end = content_length.find_last_not_of(" \n\t\r");
+        if (key == "content-length")
+        {
+            size_t start = value.find_first_not_of(" \t\n\r");
+            size_t end = value.find_last_not_of(" \n\t\r");
 
             if (start == std::string::npos || end == std::string::npos)
                 return;
 
-            content_length = content_length.substr(start, (end - start + 1));
+            value = value.substr(start, (end - start + 1));
 
-            std::stringstream stream(content_length);
+            std::stringstream stream(value);
             stream >> total_size;
         }
     };
 
     httpClient.download(url, data_callback, header_callback);
+    
+    //debug 
+    std::cout << "downloaded: " << downloaded_bytes << " of total of " << total_size << " bytes\n";
 }
