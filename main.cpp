0e5098084d3f70e712dae53b4785761312b431fb
Andrii <andrii@Andriis-MacBook-Pro-2.local>
2026-04-19 21:09:05 +0200

    feat(core): FileWriter refactor -> added write_at method, preallocate method

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 int main(int argc, char* argv[])
 {
+    /*
     if (argc < 3)
     {
         std::cerr << "Usage: ./build/downloader <URL> <output_file>";
@@ -22,6 +23,12 @@ int main(int argc, char* argv[])
         std::cerr << "Error: " << e.what() << "\n";
         return 1;
     }
+    */
+
+    FileWriter fw("test_file.txt");
+
+    fw.write_at("AAA", 3, 0);
+    fw.write_at("BBB", 3, 3);
 
     return 0;
 }
