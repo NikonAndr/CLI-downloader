7ec9528748e1343cbcde7678dba9afb6ff9d32bb
Andrii <andrii@Andriis-MacBook-Pro-2.local>
2026-04-19 18:15:29 +0200

    feat(core) Downoader refactor: added multi-thread/single_thread decision

--- src/core/download/Downloader.cpp
+++ src/core/download/Downloader.cpp
@@ -5,21 +5,16 @@
 
 void Downloader::download(const std::string& url, const std::string& output_path)
 {
-    FileWriter fw(output_path);
     HttpClient httpClient;
 
     size_t total_size = 0;
     size_t downloaded_bytes = 0;
+    bool range_supported = false;
+    long head_request_status = 0;
+    long body_request_status = 0;
 
-    auto data_callback = [&fw, &downloaded_bytes, &total_size](const char* data, size_t size)
-    {
-        downloaded_bytes += size;
-        fw.write(data, size);
-        //Debug
-        std::cout << "Downloaded: " << downloaded_bytes << "/" << total_size << " bytes\n";
-    };
-
-    auto header_callback = [&total_size](const char* buffer, size_t size)
+   std::function<void(const char*, size_t)> header_callback = 
+        [&](const char* buffer, size_t size)
     {
         std::string buffer_str(buffer, size);
         
@@ -35,23 +30,60 @@ void Downloader::download(const std::string& url, const std::string& output_path
             c = tolower(static_cast<unsigned char>(c));
         }
 
+        size_t start = value.find_first_not_of(" \t\n\r");
+        size_t end = value.find_last_not_of(" \n\t\r");
+
+        if (start != std::string::npos && end != std::string::npos)
+        {
+            value = value.substr(start, (end - start + 1));
+        }
+
         if (key == "content-length")
         {
-            size_t start = value.find_first_not_of(" \t\n\r");
-            size_t end = value.find_last_not_of(" \n\t\r");
-
-            if (start == std::string::npos || end == std::string::npos)
-                return;
-
-            value = value.substr(start, (end - start + 1));
-
             std::stringstream stream(value);
             stream >> total_size;
         }
+        else if (key == "accept-ranges")
+        {
+            if (value == "bytes")
+            {
+                range_supported = true;
+            }
+        }
     };
 
-    httpClient.download(url, data_callback, header_callback);
+    head_request_status = httpClient.request(HttpMethod::HEAD, url, nullptr, &header_callback, -1, -1);
     
+    if (head_request_status >= 400)
+    {
+        throw std::runtime_error("ERROR [Downloader]: head req status" + std::to_string(head_request_status));
+    }
     //debug 
-    std::cout << "Downloaded: " << downloaded_bytes << " of total " << total_size << " bytes\n";
+    std::cout << "Total_size: " << total_size << "\nrange_support : " << (range_supported ? "true" : "false") << std::endl;
+
+    if (total_size > 0 && range_supported)
+    {
+        std::cout << "MULTI-THREAD download\n";
+    }
+    else
+    {
+        FileWriter fw(output_path);
+
+        std::function<void(const char*, size_t)> data_callback =
+        [&](const char* data, size_t size)
+        {
+            downloaded_bytes += size;
+            fw.write(data, size);
+            //Debug
+            std::cout << "Downloaded: " << downloaded_bytes << "/" << total_size << " bytes\n";
+        };
+
+        body_request_status = httpClient.request(HttpMethod::GET, url, &data_callback, nullptr, -1, -1);
+
+        if (body_request_status >= 400)
+        {
+            throw std::runtime_error("ERROR [Downloader]: body req status" + std::to_string(body_request_status));
+        }
+    }
+
 }
